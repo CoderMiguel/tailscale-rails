9427de917ffe6ada8c57b05cfa3fbffc8cbdef0e
CoderMiguel <52347827+CoderMiguel@users.noreply.github.com>
2025-08-18 15:52:14 -0400

    add Tailscale::Status.backend_state

--- lib/tailscale/status.rb
+++ lib/tailscale/status.rb
@@ -4,6 +4,11 @@ module Tailscale
       def as_json
         JSON.parse(`tailscale status --json`)
       end
+
+      def status_attribute
+        as_json[__callee__.classify]
+      end
+      alias backend_state status_attribute
     end
   end
 end
