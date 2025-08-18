99799ecc52c3626c153e2cee106aaeefca1db833
CoderMiguel <52347827+CoderMiguel@users.noreply.github.com>
2025-08-18 14:36:05 -0400

    initial push

---
+++ Gemfile
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in tailscale-rails.gemspec
+gemspec
+
+# gem "irb"
+# gem "rake", "~> 13.0"
+
---
+++ spec/tailscale/rails_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Tailscale::Rails do
+  it "has a version number" do
+    expect(Tailscale::Rails::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
